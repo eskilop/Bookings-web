a715f75eefa939fa0d283f613eec5db338d0a309
Eskilop <eskilop@eskilop.it>
2018-12-28 16:27:49 +0100

    Added history module

---
+++ bookings/app/core/history/history.component.js
@@ -0,0 +1,9 @@
+angular.
+  module('history').
+  component('history', {
+    templateUrl: "core/history/history.template.html",
+    controller: ['$cookies', function($cookies) {
+      var self = this;
+      
+    }]
+  })
